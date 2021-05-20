5b6a073ef094e9232a08850b0ddea649e7614f7d
krushita3010 <krushitapatel3010@gmail.com>
2021-05-20 11:41:02 +0530

    Dictaphone improvised

--- src/Dictaphone.js
+++ src/Dictaphone.js
@@ -1,20 +1,58 @@
-import React from 'react'
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import React, { useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
-const Dictaphone = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition()
+function Dictaphone() {
+  const {
+    transcript,
+    interimTranscript,
+    finalTranscript,
+    resetTranscript,
+    listening,
+  } = useSpeechRecognition();
 
+  useEffect(() => {
+    if (finalTranscript !== "") {
+      console.log("Got final result:", finalTranscript);
+    }
+  }, [interimTranscript, finalTranscript]);
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return null
+    return null;
   }
 
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    console.log(
+      "Your browser does not support speech recognition software! Try Chrome desktop, maybe?"
+    );
+  }
+  const listenContinuously = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "en-GB",
+    });
+  };
   return (
     <div>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <div>
+        <span>Listening: {listening ? "on" : "off"}</span>
+        <div>
+          <button type="button" onClick={resetTranscript}>
+            Reset
+          </button>
+          <button type="button" onClick={listenContinuously}>
+            Listen
+          </button>
+          <button type="button" onClick={SpeechRecognition.stopListening}>
+            Stop
+          </button>
+        </div>
+      </div>
+      <div>
+        <span>{transcript}</span>
+      </div>
     </div>
-  )
+  );
 }
-export default Dictaphone
+
+export default Dictaphone;
